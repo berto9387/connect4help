ad44ef3c1e51ee1824e01d50685a48bc8de95107
LEO\Leo <leomaltese95@gmail.com>
2021-01-17 13:32:10 +0100

    update javascript/html chat

--- rest-controller/src/main/webapp/js/chat.js
+++ rest-controller/src/main/webapp/js/chat.js
@@ -3,6 +3,7 @@ window.addEventListener("load",loadContact,false);
 var idUser;
 
 function loadContact(){
+    document.getElementById("sendMessage").addEventListener("click", sendMessage, false);
     var myHeaders = new Headers();
     //myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", "Bearer "+localStorage.getItem("token").toString());
@@ -39,7 +40,8 @@ function createContact(result,role){
             div2.className="user_info";
                 var span=document.createElement("span");
                 if(role=="P") {
-                    id=result.requesterUser;
+                    console.log(result)
+                    id=result.requestUser; //Errore nel nome(RISOLTO)
                     nome=result.nameRequester;
                     span.textContent = result.nameRequester + " " + result.surnameRequester;
                 }else {
@@ -56,6 +58,7 @@ function createContact(result,role){
             idUser=id_user;
             var chatWith=document.getElementById("chatWith");
             chatWith.textContent="Chat with "+nome;
+            console.log(idUser)
 
         };
     }(id,nome);
@@ -65,14 +68,62 @@ function createContact(result,role){
 function clearElem(elem){
     if(elem.firstChild==null)
         return;
+
     elem.firstChild.remove();
     clearElem(elem);
 }
 function deleteChat(id_user){
+
+    console.log(id_user)
     if(id_user==idUser){
         return;
     }
     var chat=document.getElementById("chat");
+
     clearElem(chat);
 
+}
+
+function sendMessage(){
+    var messageBox = document.getElementById("textMessage");
+    var message = messageBox.value;
+
+    if(message.length == 0)
+        return;
+
+    var chatBox = document.getElementById("chat");
+
+    var messageStyle = createMessageSent(message);
+
+    chatBox.appendChild(messageStyle);
+
+    messageBox.value="";
+
+
+}
+
+function createMessageSent(message){
+    var timeStamp = new Date();
+    var divMessageBox = document.createElement("div");
+    divMessageBox.setAttribute("class", "d-flex justify-content-end mb-4");
+
+        var divMessageValue = document.createElement("div");
+        divMessageValue.setAttribute("class", "msg_cotainer_send");
+
+            var content = document.createTextNode(""+message);
+
+
+
+            var spanTime = document.createElement("span")
+            spanTime.setAttribute("class", "msg_time_send")
+                var contentSpan = document.createTextNode(""+timeStamp.getHours()+":"+timeStamp.getMinutes()
+                    +", "+timeStamp.getDate()+"/"+(timeStamp.getMonth()+1))
+            spanTime.appendChild(contentSpan)
+
+        divMessageValue.appendChild(content);
+        divMessageValue.appendChild(spanTime);
+    divMessageBox.appendChild(divMessageValue);
+    //divMessageBox.appendChild(spanTime);
+
+    return divMessageBox;
 }
