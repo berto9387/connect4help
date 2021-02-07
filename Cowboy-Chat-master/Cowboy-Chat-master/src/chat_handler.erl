6a4e33c102dd9345376b7a0392cd645479855435
berto <berto9387@gmail.com>
2021-02-07 12:00:40 +0100

    fix

---
+++ Cowboy-Chat-master/Cowboy-Chat-master/src/chat_handler.erl
@@ -0,0 +1,52 @@
+-module(chat_handler).
+-export([init/2]).
+-export([websocket_init/1]).
+-export([websocket_handle/2]).
+-export([websocket_info/2]).
+-export([terminate/3]).
+
+init(Req, Opts) ->
+	{cowboy_websocket, Req, Opts}.
+
+websocket_init(State) ->
+	self() ! {send_message, self(), "Sei entrato: inserisci l'id"},
+  {[], State}. 
+
+
+websocket_handle({text, Msg}, State) ->
+  Details=string:lexemes(Msg, "-->"), % msg
+  
+  NickReciver=lists:last(Details),
+  Msg_User=hd(Details),
+  if 
+    Msg_User == <<"!PING">> ->
+      S=State, 
+      ok;
+    Msg_User == <<"!ENTER">> ->
+      S=NickReciver,
+      chat_server:enter({self(),NickReciver});
+    Msg_User == <<"!EXIT">> -> 
+      S=State,
+      chat_server:leave(self());
+    true -> 
+      S=State,
+      chat_server:send_message({self(),{NickReciver,S}},Msg_User)
+  end,
+  {ok, S};
+websocket_handle(_Data, State) ->
+  {[], State}.
+  
+websocket_info({send_message, _ServerPid, Msg}, State) ->
+    {reply, {text, Msg}, State};
+websocket_info({timeout, _Ref, Msg}, State) ->
+	%erlang:start_timer(1000, self(), <<"How' you doin'?">>),
+	{[{text, Msg}], State};
+websocket_info(_Info, State) ->
+  {[], State}.
+
+ 
+ terminate(_Reason, _Req, _State) ->
+   %problemi con invia messaggio.
+    chat_server:leave(self()),
+    ok.
+
